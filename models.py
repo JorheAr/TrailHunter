cb6197da6e1f67a44ea1acabc4a8f92732cabd21
Jorge Arias <jariasmartin@safareyes.es>
2025-04-08 12:27:12 +0200

    ♻️ & ✨ Add client model and changes on User

--- models.py
+++ models.py
@@ -4,14 +4,34 @@ from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 class Usuario(db.Model):
+    __tablename__ = "usuarios"
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nombre = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True,  nullable=False)
+    email = db.Column(db.String(100), nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
     fecha_registro = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp())
+
+    # Rol: admin, moderador o usuario
+    rol = db.Column(db.String(20), nullable=False, default='usuario')
+
+    # Relación uno a uno con Cliente
+    cliente = db.relationship("Cliente", backref="usuario", uselist=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+class Cliente(db.Model):
+    __tablename__ = "clientes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    apellidos = db.Column(db.String(100), nullable=False)
+    fecha_nacimiento = db.Column(db.Date, nullable=False)
+
+    # Relación con usuario
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'), unique=True, nullable=False)
