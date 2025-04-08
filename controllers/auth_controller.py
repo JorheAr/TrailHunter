e4952dcc886241486603832bcb9e0fa090cf7130
Jorge Arias <jariasmartin@safareyes.es>
2025-04-08 12:26:35 +0200

    ♻️ Changes on login and register for front

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,18 +1,19 @@
 from flask import request, jsonify
 from services.auth_service import register_user, login_user
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
 
 def register():
-    """Controlador para el registro de usuario."""
     data = request.get_json()
-    return jsonify(*register_user(data))
+    response, status_code = register_user(data)
+    return jsonify(response), status_code
 
 def login():
-    """Controlador para el inicio de sesión."""
     data = request.get_json()
-    return jsonify(*login_user(data))
+    response, status_code = login_user(data)
+    return jsonify(response), status_code
 
 @jwt_required()
 def protected():
-    """Ruta protegida para probar JWT."""
-    return jsonify({"message": "Acceso permitido"}), 200
+    current_user_id = get_jwt_identity()
+    return jsonify({"message": f"Acceso concedido para el usuario con ID {current_user_id}"}), 200
